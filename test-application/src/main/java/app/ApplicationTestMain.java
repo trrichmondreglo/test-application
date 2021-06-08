50f7a96c81adec94dc2588e6ebcb395a78853de9
Richmond Reglo <richmond.reglo25@gmail.com>
2021-06-08 16:07:04 +0800

    Sample Commit

--- test-application/src/main/java/app/ApplicationTestMain.java
+++ test-application/src/main/java/app/ApplicationTestMain.java
@@ -3,6 +3,11 @@ package app;
 import dto.TestObject;
 
 public class ApplicationTestMain {
+	
+	/*
+	 * Test Application Comment
+	 * For push purpose only
+	 */
 	public static void main(String[] args) {
 		TestObject testObj = new TestObject();
 		testObj.setId(1);
